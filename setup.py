3a08bcdd8ea47aeeff3efaca97116cf31189085e
Vlasov Dmitriy <scailer@veles.biz>
2016-11-11 10:41:05 +0500

    fix setup

--- setup.py
+++ setup.py
@@ -11,22 +11,20 @@ setup(
     packages=['pg_store'],
     include_package_data=True,
     install_requires=['aiopg==0.9.2'],
-    requires=['aiopg', 'pulsar (>= 1.4.0)'],
+    requires=['aiopg', 'pulsar (>= 1.5.4)'],
 
     url='https://github.com/scailer/pulsar-postgresql',
     license='MIT license',
     description='Pulsar store for postgresql database based on aiopg',
 
-    classifiers=(
-        'Development Status :: 5 - Production/Stable',
+    classifiers=[
+        'Development Status :: 4 - Beta',
         'Environment :: Plugins',
-        'Framework :: Pulsar',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
-        'Natural Language :: English',
-    ),
+    ],
 )
